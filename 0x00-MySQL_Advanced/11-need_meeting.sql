5f7e80a663a1e5fb8642048ca04f86e98915fa3f
L <sebasgarzons88@gmail.com>
2022-08-11 22:35:38 -0500

    A script creates a view , fixed condition

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -11,4 +11,4 @@
 */
 
 CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND
-(last_meeting IS NULL OR last_meeting < DATE(CURDATE(), INTERVAL 1 MONTH));
+(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
