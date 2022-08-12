5f8d6b1b605856bc867350d3a931634024bd754b
L <sebasgarzons88@gmail.com>
2022-08-11 22:49:23 -0500

    Rank by style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,6 +7,6 @@
     Your script can be executed on any database
 */
 
-SELECT band_name, ifnull(split, 2020) - ifnull(formed, 0) AS lifespan
+SELECT band_name, ifnull(split, 2022)-ifnull(formed, 0) AS lifespan
 FROM metal_bands WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
