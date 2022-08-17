ea67923877ca04c38c4323fef112c6830bd5d03c
L <sebasgarzons88@gmail.com>
2022-08-16 20:46:28 -0500

    Update by topics, fixed how many again

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,7 +10,7 @@ def update_topics(mongo_collection, name, topics):
     - name (string) will be the school name to update
     - topics (list of strings) will be the list of topics
     approached in the school"""
-    mongo_collection.update(
+    mongo_collection.updateMany(
         { 'name': name },
         { 'set': {'topics': topics} }
     )
