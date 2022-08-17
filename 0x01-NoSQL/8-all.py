6229e467f1a0dd141057190380d94d20d4b1ea32
L <sebasgarzons88@gmail.com>
2022-08-16 20:22:24 -0500

    Find all documents in a object collection, fixed documentation

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Write a Python function that lists all documents in a collection
+"""
 def list_all(mongo_collection):
+    """list_all() Return an empty list if no document
+    in the collection"""
     return(mongo_collection.find())
